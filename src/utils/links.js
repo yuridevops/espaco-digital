2c027e4e4c9ae51ca20d788a6f2d0158639bd12d
Marcos Yuri <sicredins@gmail.com>
2021-01-12 13:23:25 -0300

    videos novos

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,44 +1,45 @@
 const links  = [
     {
-        name: 'Sicredi',
-        link: 'https://youtube.com/embed/R_zL5kRO3gk?autoplay=1',
+        name: 'Pix no Sicredi',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Com+o+Pix+no+Sicredi+%C3%A9+assim.mp4',
+        image: 'https://www.idinheiro.com.br/wp-content/uploads/2020/06/PIX.jpg'
+    },
+    {
+        name: 'Gestão de Fluxo de Caixa',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Solu%C3%A7%C3%B5es+para+otimizar+a+gest%C3%A3o+do+fluxo+de+caixa.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi.png'
     },
     {
-        name: 'Sicredi Dicas Financeiras',
-        link: 'https://youtube.com/embed/T7_8yfgWbSU?autoplay=1',
-        image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-dicas-financeiras.png'
-    },
-    {
         name: 'Sicredi Agro',
-        link: 'https://youtube.com/embed/Tp07wT0aQmM?autoplay=1',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Sim+Sicredi+Agro.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-agro.png'
     },
     {
         name: 'Sicredi PJ',
-        link: 'https://youtube.com/embed/vmv2CRw5NmM?autoplay=1',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Solu%C3%A7%C3%B5es+para+otimizar+a+gest%C3%A3o+do+fluxo+de+caixa.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-pj.png'
     },
     {
         name: 'Sicredi PF',
-        link: 'https://youtube.com/embed/im0tT7gAkF0?autoplay=1',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Sim+Sicredi+Pessoa+F%C3%ADsica.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-pf.png'
     },
     {
-        name: 'Sicredi Safra',
-        link: 'https://youtube.com/embed/Yb9gMnjDuc8?autoplay=1',
+        name: 'Maquininha de Cartão Sicredi',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Maquininha+de+cart%C3%A3o+Sicredi.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-safra.png'
     },
     {
         name: 'Sicredi Conecta',
-        link: 'https://youtube.com/embed/fPJxSeI34Vo?autoplay=1',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Sicredi+Conecta.mp4',
         image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi_conecta.png'
     },
     {
-        name: 'Sicredi União Faz a Vida',
-        link: 'https://youtube.com/embed/D5526IBQno0?autoplay=1',
-        image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi-uniao-vida.png'
-    },
+        name: 'Cooperativismo',
+        link: 'https://sicredi.s3-sa-east-1.amazonaws.com/Eu+coopero+com+a+economia+local.mp4',
+        image: 'https://storage.googleapis.com/sicredi/imagens-portal/sicredi.png'
+    }
+   
 ]
 
 export default links
